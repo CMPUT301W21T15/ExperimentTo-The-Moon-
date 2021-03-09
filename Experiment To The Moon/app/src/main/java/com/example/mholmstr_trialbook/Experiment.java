c6e9f799b132a5e623c5529bc6e7903823a9ad63
Calder Orieux <corieux@ualberta.ca>
2021-03-08 17:07:56 -0700

    Trial

--- Experiment To The Moon/app/src/main/java/com/example/mholmstr_trialbook/Experiment.java
+++ Experiment To The Moon/app/src/main/java/com/example/mholmstr_trialbook/Experiment.java
@@ -2,25 +2,26 @@ package com.example.mholmstr_trialbook;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Experiment implements Serializable {
     // the Experiment class represents an experiment within the program's collection of experiments
-    private long date;
+    private Date date;
     private String description;
     private final ArrayList<Trial> successes = new ArrayList<>();
     private final ArrayList<Trial> failures = new ArrayList<>();
 
-    public Experiment(long date, String description) {
-        setDate(date);
+    public Experiment(long inputDate, String description) {
+        setDate(inputDate);
         setDescription(description);
     }
 
     public long getDate() {
-        return date;
+        return date.getTime();
     }
 
-    public void setDate(long date) {
-        this.date = date;
+    public void setDate(long inputDate) {
+        date = new Date(inputDate);
     }
 
     public String getDescription() {
